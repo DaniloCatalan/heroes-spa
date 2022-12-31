6abcf6bec33d97ddbaf8fd00ec2980995f739372
Danilo P. Catalan <danilo.e.catalan@gmail.com>
2022-12-31 00:34:28 -0300

    add hero card

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,9 +1,9 @@
 import { HeroCard } from './HeroCard';
 import { getHeroesByPublisher } from '../helpers';
+import { useMemo } from 'react';
 
 export const HeroList = ({ pusblisher }) => {
-  const heroes = getHeroesByPublisher(pusblisher);
-  console.log(heroes);
+  const heroes = useMemo(() => getHeroesByPublisher(pusblisher, [pusblisher]));
   return (
     <div className="row row-cols-1 row-cols-md-3 g-3">
       {heroes.map((hero) => (
